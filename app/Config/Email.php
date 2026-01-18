9949a02e2481ba32013e0498f01a5b9c9b8a6702
Damar Syah Maulana <ibn.damr@gmail.com>
2026-01-18 18:36:51 +0700

    Configure Shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'hello@konstelasi.co.id';
+    public string $fromName   = 'Hello Konstelasi';
     public string $recipients = '';
 
     /**
